8e454d65cead3efd1e1f661346dc1628bf225294
alan-hsieh <alan_hsieh@trendmicro.com>
2023-12-05 18:40:33 +0900

    update code

--- function_app.py
+++ function_app.py
@@ -1,13 +1,18 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="prompt")
 def JakeTest(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    
+    result = {
+        result: "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a pers    onalized response."
+    }
     return func.HttpResponse(
-        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+        json.dumps(result),
+        mimetype="application/json",
         status_code=200
     )

7c8c2a688db169089c7efb9b59d950e017b71016
alan-hsieh <alan_hsieh@trendmicro.com>
2023-12-05 20:15:40 +0900

    get body and print

--- function_app.py
+++ function_app.py
@@ -7,6 +7,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 @app.route(route="prompt")
 def JakeTest(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+    message = req.get_body()
+    logging.info('body:' + message)
     
     result = {
         "result": "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a pers    onalized response."
